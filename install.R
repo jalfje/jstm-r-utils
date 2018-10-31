db1d5a04f4905ff0b45c3943347e5b07953d6944
Jamie St Martin <jamiestm9@hotmail.com>
2018-10-31 12:24:23 -0700

    Updated install script

--- install.R
+++ install.R
@@ -1,10 +1,18 @@
 #!/usr/local/bin/Rscript
 
+# install.R
+# Installs the `devtools` package if it isn't already installed, then
+# uses `devtools` to installs the jstm package. Note that this may
+# open a prompt requesting the user to choose which CRAN mirror to
+# use, and may also prompt for a library location. Use in automated
+# scripts at your own risk!
+# Execute with `Rscript install.R`
+
 if (!suppressWarnings(suppressMessages(require("devtools")))) {
     install.packages("devtools")
     library("devtools")
 }
 
-document()
-install(".")
+devtools::document()
+devtools::install(".")
 
